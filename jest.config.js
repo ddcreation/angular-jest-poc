839589205279ecc64c8ec7d7ecda320b2a0a6191
David Daupeyroux <david.daupeyroux@one-insurance.com>
2020-08-18 14:42:48 +0200

    Improve jest config and move setup

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,15 @@
-const jestPreset = require("jest-preset-angular/jest-preset");
-const { globals } = jestPreset;
-const tsjest = globals["ts-jest"];
-
-const tsjestOverrides = {
-  ...tsjest,
-  tsConfig: "<rootDir>/tsconfig.spec.json",
-};
-const globalOverrides = {
-  ...globals,
-  "ts-jest": { ...tsjestOverrides },
-};
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig");
 
 module.exports = {
-  ...jestPreset,
-  globals: { ...globalOverrides },
   preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
+  roots: ["<rootDir>/src/"],
+  testMatch: ["**/+(*.)+(spec).+(ts)"],
+  setupFilesAfterEnv: ["<rootDir>/src/test.ts"],
+  collectCoverage: true,
+  coverageReporters: ["html"],
+  coverageDirectory: "coverage/my-app",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: "<rootDir>/",
+  }),
 };
