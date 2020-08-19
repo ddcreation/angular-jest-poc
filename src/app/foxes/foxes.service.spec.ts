bd609f478d860cf68fa8c74952d95c70591c0e6b
David Daupeyroux <david.daupeyroux@one-insurance.com>
2020-08-19 10:25:53 +0200

    Fox service add function

--- src/app/foxes/foxes.service.spec.ts
+++ src/app/foxes/foxes.service.spec.ts
@@ -13,4 +13,29 @@ describe('FoxesService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('initial empty state', () => {
+    expect(service.foxes.length).toBe(0);
+  });
+
+  it('should add fox', () => {
+    const newFoxName = 'testFox';
+    const addedFox = service.add(newFoxName);
+
+    // Check new property length
+    expect(service.foxes.length).toBe(1);
+
+    // Sample fox
+    const sampleFox = {
+      id: 1,
+      name: newFoxName,
+    };
+
+    // Compare datas
+    // Received from the add function response:
+    expect(addedFox).toEqual(sampleFox);
+
+    // In property:
+    expect(service.foxes[0]).toEqual(sampleFox);
+  });
 });
--- src/app/foxes/foxes.service.ts
+++ src/app/foxes/foxes.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
+export interface Fox {
+  id: number;
+  name: string;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoxesService {
+  public foxes: Fox[] = [];
 
-  constructor() { }
+  public add(foxname: string): Fox {
+    const newFox = {
+      id: this.foxes.length + 1,
+      name: foxname,
+    };
+
+    this.foxes.push(newFox);
+    return newFox;
+  }
 }
